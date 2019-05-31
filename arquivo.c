f923736fb0c4247e817cb186f6ce5e63a737140c
willian-silva <48417209+willian-silva@users.noreply.github.com>
2019-05-31 20:51:30 -0300

    exercício 1 (arquivos)

---
+++ arquivo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main(void){
+    char str[25];
+    printf("Digite a string: ");
+    scanf("%s",str);
+    int p=strlen(str);
+    FILE*arq=fopen("/home/ime/Área de Trabalho/antarqu.txt","w+");
+    for(int i=0;i<p;i++){
+        fputc(str[i],arq);
+    }
+    fclose(arq);
+}
+    
+        
+
