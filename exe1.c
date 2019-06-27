ad219523b1bc010933cdd5b6ae357e1e95f6bb56
willian-silva <48417209+willian-silva@users.noreply.github.com>
2019-06-27 15:49:12 -0300

    lista encadeada

--- exe1.c
+++ exe1.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-struct personagem{
-    int forca;
-    int energia;
-    int experiencia;
-    };
+struct caixa{
+    int valor;
+    struct caixa *prox;
+};
 void main(void){
-    struct personagem p1;
-    p1.forca=10;
-    p1.energia=50;
-    p1.experiencia=30;
-    struct personagem p2={18,20,25};
-    printf("%d %d %d\n%d %d %d\n",p1.forca,p1.energia,p1.experiencia,p2.forca,p2.energia,p2.experiencia);
-    }
+    struct caixa c5={5,NULL};
+    struct caixa c4={7,&c5};
+    struct caixa c3={9,&c4};
+    struct caixa c2={3,&c3};
+    struct caixa c1={1,&c2};
+    printf("%d->%d->%d->%d->%d\n",c1.valor,c2.valor,c3.valor,c4.valor,c5.valor);
+}
     
