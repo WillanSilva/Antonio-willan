2be99a502d4751947c481932a381b374f6307240
willian-silva <48417209+willian-silva@users.noreply.github.com>
2019-06-30 16:09:33 -0300

    lista encadeada

--- exe5.c
+++ exe5.c
@@ -1,23 +1,83 @@
 #include<stdio.h>
-struct ponto{
-    int x;
-    int y;
-    int z;
-    };
-void soma(struct ponto *p){
-    p->z=p->z+10;
+struct caixa{
+    int valor;
+    struct caixa *prox;
+};
+int contem(struct caixa *l,int v){
+    while(l->prox!=NULL){
+        if (l->valor==v){
+            break;
+        }
+        l=l->prox;
     }
+    if (l->valor==v){
+        return v;
+    }
+    else{
+        return -1;
+   }
+}
+void imprime(struct caixa *s){
+    while(s->prox!=NULL){
+        printf("%d->",s->valor);
+        s=s->prox;
+    }
+    printf("%d\n",s->valor);
+}
+struct caixa* remover(struct caixa* lista, int valor){
+        int n;
+        n=contem(lista,valor);
+        if (n!=-1){
+            while (lista->prox!=NULL){
+                if (lista->valor==valor){
+                    lista->valor=lista->prox->valor;
+                    lista->prox=lista->prox->prox;
+                    break;
+                }
+                else if(lista->prox->valor==valor){
+                     if(lista->prox->prox==NULL){
+                           lista->prox=NULL;
+                            break;
+                    }
+                    else{
+                         lista->prox=lista->prox->prox;
+                         break;
+                    }
+                }
+                lista=lista->prox;       
+                      
+            }
+            return  lista;
+        }
+       else{
+            printf("valor %d não contem na lista!\n",valor);
+            return lista;
+       }
+}
 void main(void){
-    struct ponto v1;
-    struct ponto v2;
-    struct ponto v3;
-    v1.x=1;v1.y=0;v1.z=5;
-    v2.x=3;v2.y=3;v2.z=3;
-    v3.x=0;v3.y=10;v3.z=0;
-    printf("%d %d %d\n",v1.y,v2.y,v3.y);
-    soma(&v1);soma(&v2);soma(&v3);
-    printf("v2= %d %d %d\n",v2.x,v2.y,v2.z);
+    struct caixa c[10];
+     for(int i=0;i<10;i++){
+        if (i==9){
+            printf("Digite o valor da caixa: ");
+            scanf("%d",&c[i].valor);
+            c[i].prox=NULL;
+        }
+        else{
+            printf("Digite o valor da caixa: ");
+            scanf("%d",&c[i].valor);
+            c[i].prox=(&c[i+1]);
+        }
     }
+    imprime(&c[0]);
+    int n;
+    printf("Digite o elemento a ser removido: ");
+    scanf("%d",&n);
+    struct caixa *s=&c[0];
+    s=remover(&c[0],n);
+    imprime(&c[0]);
+}
     
     
-
+            
+            
+            
