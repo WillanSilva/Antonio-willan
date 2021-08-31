1fc618db35cfd6eb44d1a74a7c0bd9a01c736dd5
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-08-31 12:05:14 -0300

    Update EXE5 (arq).c

--- Programas em C/EXE5 (arq).c
+++ Programas em C/EXE5 (arq).c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<string.h>
+void escreva_string(FILE* f, char* str){
+    f=fopen("arq2.txt","w");
+    int tam=strlen(str);
+    for (int i=0;i<tam;i++){
+        fputc(str[i],f);
+    }
+    fclose(f);
+}
+void main(void){
+    FILE* m;
+    char stri[25];
+    printf("Digite uma string: ");
+    scanf("%s",stri);
+    escreva_string(m,stri);
+}
