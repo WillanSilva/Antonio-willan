84abcf29255042d22e719eb1baa1fd753c2c3ca6
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-08-31 12:01:24 -0300

    Create EXE3.c

---
+++ Programas em C/EXE3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+struct caixa{
+    int valor;
+    struct caixa * prox;
+};
+void exibe(struct caixa *p){
+    while (p->prox!=NULL){
+        printf("%d->",p->valor);
+        p=p->prox;
+    }
+    printf("%d",p->valor);
+    printf("\n");
+}         
+void main(void){
+    struct caixa c5={5,NULL};
+    struct caixa c4={7,&c5};
+    struct caixa c3={9,&c4};
+    struct caixa c2={3,&c3};
+    struct caixa c1={1,&c2};
+    struct caixa* cabeca;
+    exibe(&c1);
+    cabeca=&c1;
+    cabeca->prox->prox=&c4;
+    exibe(&c1);
+    cabeca=&c3;
+    cabeca->prox=&c1;
+    cabeca->prox->prox->prox=&c4;
+    exibe(&c3);
+    
+}
